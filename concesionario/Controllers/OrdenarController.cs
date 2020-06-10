c147abbda4ab272e667ee161b6a0ae3747e62099
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-06-10 00:25:33 -0700

    subir

--- concesionario/Controllers/OrdenarController.cs
+++ concesionario/Controllers/OrdenarController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using concesionario.Models;
 namespace concesionario.Controllers
 {
     public class OrdenarController : Controller
     {
         // GET: Ordenar
+        private ConsecionarioDeAutosEntities db = new ConsecionarioDeAutosEntities();
         public ActionResult Index( int IdAuto)
         {
+            //ViewBag.Performance = (from p in db.Performance 
+            //                       select new 
+            //                       { 
+            //                           Id
+            //                       }).ToList();
             return View("Index");
         }
 
